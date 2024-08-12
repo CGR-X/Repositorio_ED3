1da86b07c9d2e20f73a47dd89bb98876ed4311e5
Enzo Verstraete <158090803+CGR-X@users.noreply.github.com>
2024-08-12 14:12:54 -0300

    Add files via upload

---
+++ tarea1/tarea1.c
@@ -0,0 +1,10 @@
+// #include <stdio.h> // añadimos biblioteca
+int main(int argc, char** argv)
+{
+    printf("Primer programa en C\n");
+    printf("Tarea 1 clase inicial\n");
+
+    printf("numero de la suerte: %d\n",199);
+
+    return 0;
+}
